ab7d967dad890dacc553e22fce5794fefe59fdf1
Jose Trujillo <jmigueltrujillo@gmail.com>
2018-06-11 10:31:33 -0600

    update app.js with original code

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const fixtures = require('./data/fixtures')
-const cors = require("cors")
-app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
